ac0c3fda99d4646a326406b6f0435c311ae849a0
BENCODET <1940@holbertonschool.com>
2020-06-10 19:38:35 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of a char: %d byte(s)\n", sizeof(ch));
 printf("Size of an int: %d byte(s)\n", sizeof(in));
 printf("Size of a long int: %d byte(s)\n", sizeof(li));
 printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
- printf("Size of a float: %d byte(s)\n", sizeof(fl));
+printf("Size of a float: %d byte(s)\n", sizeof(fl));
 return (0);
 }
