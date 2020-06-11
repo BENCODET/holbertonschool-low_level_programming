afe935e8807a2600d0d4e4448d081bbadf28e1f2
BENCODET <1940@holbertonschool.com>
2020-06-11 20:16:28 +0000

    print base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+char l;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+	}
+	for (l = 'a'; l <= 'f' ; l++)
+		putchar(l);
+	return (0);
+}
