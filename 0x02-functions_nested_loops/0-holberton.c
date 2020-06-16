f63a5ee7ed146614fa5e6836b16dfaf53925fb6c
BENCODET <1940@holbertonschool.com>
2020-06-16 08:58:49 +0000

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,17 +6,16 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-putchar ('H');
-putchar ('o');
-putchar ('l');
-putchar ('b');
-putchar ('e');
-putchar ('r');
-putchar ('t');
-putchar ('o');
-putchar ('n');
-putchar ('\n');
-return (0);
+	putchar ('H');
+	putchar ('o');
+	putchar ('l');
+	putchar ('b');
+	putchar ('e');
+	putchar ('r');
+	putchar ('t');
+	putchar ('o');
+	putchar ('n');
+	putchar ('\n');
+	return (0);
 }
