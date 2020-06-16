2dd502bb7d1919b703aaf8dc583c7e89c83686bb
BENCODET <1940@holbertonschool.com>
2020-06-16 21:29:58 +0000

    checks

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
  * _isalpha - yes
- *@c: intege
+ *@c: int
  * Return: 0 or 1
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
