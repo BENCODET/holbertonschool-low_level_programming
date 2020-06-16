1682fc840e66c3b2f58b35ea303e507179cf0c0e
BENCODET <1940@holbertonschool.com>
2020-06-16 09:17:58 +0000

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 /**
- * main - Entry point
+ * main -print holberton
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	_putchar ('H');
-	_putchar ('o');
-	_putchar ('l');
-	_putchar ('b');
-        _putchar ('e');
-	_putchar ('r');
-	_putchar ('t');
-	_putchar ('o');
-	_putchar ('n');
-	_putchar ('\n');
-	return (0);
+_putchar ('H');
+_putchar ('o');
+_putchar ('l');
+_putchar ('b');
+_putchar ('e');
+_putchar ('r');
+_putchar ('t');
+_putchar ('o');
+_putchar ('n');
+_putchar ('\n');
+return (0);
 }
