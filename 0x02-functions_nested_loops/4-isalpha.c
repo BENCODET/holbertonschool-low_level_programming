df4f4bcb80d5362cd7836b6d6ec7b375f142f2fe
BENCODET <1940@holbertonschool.com>
2020-06-16 21:25:22 +0000

    checks

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,17 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _isalpha - yes
+ *@c: intege
+ * Return: 0 or 1
  */
 int _isalpha(int c);
 {
-	if (c >= a && c <= z || c >= A )
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
