1b0d1aed60c2459d984635f801f9cdba9fb5078d
BENCODET <1940@holbertonschool.com>
2020-06-18 21:15:43 +0000

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+include "holberton.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: triangle's size
+ * Return: nothing
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			k = size - i - 1;
+			if (j < k)
+			{
+				_putchar(' ');
+			}
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
