92665f3ccec36fc0549de96ecce5fdc190385788
BENCODET <1940@holbertonschool.com>
2020-06-23 21:23:08 +0000

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *rev_string - function that reverses a string
+ *@s: string
+ *
+ */
+void rev_string(char *s)
+{
+	char n;
+	int i, j;
+
+	i = 0;
+	while (s[i + 1] != '\0')
+	{
+		i++;
+	}
+
+	j = i;
+	i = 0;
+	while (i < j / 2 + 1)
+	{
+		n = s[i];
+		s[i] = s[j - i];
+		s[j - i] = c;
+		i++;
+	}
+}
