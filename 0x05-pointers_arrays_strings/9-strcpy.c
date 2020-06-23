39034b4919d347f94aaeb64bcd1ccc8838422a74
BENCODET <1940@holbertonschool.com>
2020-06-23 22:38:27 +0000

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *_strcpy - copies the string pointed to by src, including the terminating null
+ *@src: src
+ *@dest: dest
+ *Return: desty
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i , j;
+
+	i=0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j <= i)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	return (dest);
+}
