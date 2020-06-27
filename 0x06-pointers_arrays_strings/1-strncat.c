57dc7dcb732cf2e864de71fa830a5203f53425cc
BENCODET <1940@holbertonschool.com>
2020-06-27 12:54:08 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *_strncat - add str2 into str1 and stop at n characeter
+ *@src: input
+ *@dest: input
+ *@n: input;
+ *Return: a string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *str = dest;
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0' && i != n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	return (str);
+}
