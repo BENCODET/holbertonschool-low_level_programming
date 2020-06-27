d60e2a2c46a6a31f640422e1fd5460be0181dad7
BENCODET <1940@holbertonschool.com>
2020-06-27 13:46:04 +0000

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *string_toupper - changes all lowercase letters of a string to uppercase
+ *@s: input
+ *Return: string
+ */
+char *string_toupper(char *s)
+{
+	char *d = s;
+
+	while (*s != '\0')
+	{
+		if (*s >= 97 && *s <= 122)
+			*s -= 32;
+		else
+			s++;
+	}
+	return (d);
+}
