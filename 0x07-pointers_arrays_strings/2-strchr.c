c1957644168c2dbe733bd23366677709d21f79e6
BENCODET <1940@holbertonschool.com>
2020-06-29 16:27:11 +0000

    locate caracter in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strchr - Locates a character in a string
+ * @str: string
+ * @c: character
+ *
+ * Return: Return a pointer to the fist occurence of c
+ */
+char *_strchr(char *str, char c)
+{
+	while (*str++)
+	{
+		if (*str == c)
+			return (str);
+	}
+	return (NULL);
+}
