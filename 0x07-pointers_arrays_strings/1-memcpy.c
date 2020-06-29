df36b28d58a22e52ed5116d9b1d0524be4f32f9f
BENCODET <1940@holbertonschool.com>
2020-06-29 16:15:04 +0000

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_memcpy - copies memory area
+ *@dest: input
+ *@src: input
+ *@n: input
+ *Return: string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+		*(dest + i) = *(src + i);
+
+	return (dest);
+}
