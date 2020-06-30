d71e67e2ab6a023a1d606a9d36af2b705e01a59f
BENCODET <1940@holbertonschool.com>
2020-06-30 20:57:15 +0000

    string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strstr - Locates a substring
+ * @haystack: string
+ * @needle: string
+ *
+ * Return: A pointer to the beginning of occurance, NULL otherwise
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	while (haystack[i] != '\0')
+	{
+		if (needle[j] != '\0' && haystack[i] == needle[j])
+			j++;
+		else
+			break;
+		i++;
+	}
+	if (needle[j] == '\0')
+		return (haystack + (i - j));
+	return (0);
+}
